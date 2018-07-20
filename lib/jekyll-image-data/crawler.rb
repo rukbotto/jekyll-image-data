05e1fa4f0198d39542d4490d52aa4b7973f0c50c
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2018-07-19 19:51:44 -0500

    Rewrite plugin code completely
    This addresses an issue that prevented to use image data in layouts and
    include files because it was generated after the rendering process had
    finished.

--- lib/jekyll-image-data/crawler.rb
+++ lib/jekyll-image-data/crawler.rb
@@ -1,23 +1,41 @@
-require "nokogiri"
-
 module JekyllImageData
   class Crawler
+    def initialize
+      src = %r{(?:https|http|mailto)?(?:\:/)?/\S+}
+      alt = %r{[[[:alnum:]][[:space:]]-]+}
+
+      md_image = %r{!\[(.*)\]\((.*)\)}
+      md_image_ref = %r{!\[(.*)\]\[(.*)\]}
+      html_image = %r{<img.*(src="(#{src})".*alt="(#{alt})"|alt="(#{alt})".*src="(#{src})"|src="(#{src})")}
+      include_image = %r{\{\%\s*include\s*image.(liquid|html)\s*(src="(#{src})".*alt="(#{alt})"|alt="(#{alt})".*src="(#{src})")}
+
+      @image = %r{#{md_image}|#{md_image_ref}|#{html_image}|#{include_image}}
+      @image_ref = %r{\[(.*)\]:\s*(\S*)}
+    end
+
     def crawl(content, config)
       images = []
-      exclude = config.dig("image_data", "exclude") || []
-      html = Nokogiri::HTML(content)
-      html.xpath("//img").each do |item|
-        src = item.xpath("@src").first
-        alt = item.xpath("@alt").first
-        excluded = exclude.select do |exclude_item|
-          src.value.include?(exclude_item)
-        end
-        next unless excluded.empty?
-        images << {
-          "url" => src ? src.content : "",
-          "alt" => alt ? alt.content : ""
-        }
+      exclude = config.dig("image_data", "exclude") || ""
+      exclude = Regexp.new(exclude)
+
+      content.scan(@image) do |match|
+        src = match[1] || match[5] || match[8] || match[9] || match[12] || match[15] || ""
+        alt = match[0] || match[2] || match[6] || match[7] || match[13] || match[14] || ""
+        ref = match[3] || ""
+        images << { "url" => src, "alt" => alt, "ref" => ref }
       end
+
+      content.scan(@image_ref) do |match|
+        images.each do |image|
+          image["url"] = match[1] if image["ref"] == match[0]
+        end
+      end
+
+      images.each do |image|
+        image.delete("ref")
+        images.delete(image) if exclude.match?(image["url"])
+      end
+
       images
     end
   end
